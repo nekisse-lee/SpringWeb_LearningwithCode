81683f70e786188fc42f286e29dabbdb6c8e8360
nekisse <lsh891224@gmail.com>
2018-01-06 21:34:00 +0900

    기능구현중..

--- ex01/src/main/java/org/zerock/controller/BoardController.java
+++ ex01/src/main/java/org/zerock/controller/BoardController.java
@@ -50,5 +50,30 @@ public class BoardController {
 	public void read(@RequestParam("bno") int bno, Model model) throws Exception{
 		model.addAttribute(service.read(bno));
 	}
+		
+	@RequestMapping(value = "/remove", method = RequestMethod.POST)
+	public String remove(@RequestParam("bno") int bno, RedirectAttributes rttr) throws Exception{
+		service.remove(bno);
+		
+		rttr.addFlashAttribute("msg", "SUCCESS");
+		
+		return "redirect:/board/listAll";
+	}
+	
+	@RequestMapping(value = "/modify", method = RequestMethod.GET)
+	public void modifyGET(int bno, Model model) throws Exception{
+		
+		model.addAttribute(service.read(bno));
+	}
+	
+	@RequestMapping(value = "modify", method = RequestMethod.POST)
+	public String modifyPOST(BoardVO board, RedirectAttributes rttr) throws Exception{
+		logger.info("mod post...");
+		
+		service.modify(board);
+		rttr.addFlashAttribute("msg", "SUCCESS");
+		
+		return "redirect:/board/listAll";
+	}
 	
 }

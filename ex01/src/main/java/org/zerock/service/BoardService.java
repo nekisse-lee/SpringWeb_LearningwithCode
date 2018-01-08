792b5974f2b8f572d80a5a4a716eface8d57b5bd
nekisse <lsh891224@gmail.com>
2018-01-08 15:39:21 +0900

    페이징 처리중

--- ex01/src/main/java/org/zerock/service/BoardService.java
+++ ex01/src/main/java/org/zerock/service/BoardService.java
@@ -3,6 +3,7 @@ package org.zerock.service;
 import java.util.List;
 
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 public interface BoardService {
 	
@@ -16,5 +17,6 @@ public interface BoardService {
 	
 	public List<BoardVO> listAll() throws Exception;
 	
+	public List<BoardVO> listCriteria(Criteria cri) throws Exception;
 
 }
--- ex01/src/test/java/org/zerock/test/BoardDAOTest.java
+++ ex01/src/test/java/org/zerock/test/BoardDAOTest.java
@@ -2,6 +2,8 @@ package org.zerock.test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.junit.Test;
@@ -11,49 +13,72 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.persistence.BoardDAO;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-		locations = {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml" })
 public class BoardDAOTest {
 
-	  @Inject
-	  private BoardDAO dao;
+	@Inject
+	private BoardDAO dao;
 
-	  private static Logger logger = LoggerFactory.getLogger(BoardDAOTest.class);
+	private static Logger logger = LoggerFactory.getLogger(BoardDAOTest.class);
 
-	  @Test
-	  public void testCreate() throws Exception {
+	@Test
+	public void testCreate() throws Exception {
 
-	    BoardVO board = new BoardVO();
-	    board.setTitle("새로운 글을 넣습니다. ");
-	    board.setContent("새로운 글을 넣습니다. ");
-	    board.setWriter("user00");
-	    dao.create(board);
-	  }
+		BoardVO board = new BoardVO();
+		board.setTitle("새로운 글을 넣습니다. ");
+		board.setContent("새로운 글을 넣습니다. ");
+		board.setWriter("user00");
+		dao.create(board);
+	}
 
-	  @Test
-	  public void testRead() throws Exception {
+	@Test
+	public void testRead() throws Exception {
 
-	    logger.info(dao.read(1).toString());
-	  }
+		logger.info(dao.read(1).toString());
+	}
 
-	  @Test
-	  public void testUpdate() throws Exception {
+	@Test
+	public void testUpdate() throws Exception {
 
-	    BoardVO board = new BoardVO();
-	    board.setBno(1);
-	    board.setTitle("수정된 글입니다.");
-	    board.setContent("수정 테스트 ");
-	    dao.update(board);
-	  }
+		BoardVO board = new BoardVO();
+		board.setBno(1);
+		board.setTitle("수정된 글입니다.");
+		board.setContent("수정 테스트 ");
+		dao.update(board);
+	}
 
-	  @Test
-	  public void testDelete() throws Exception {
+	@Test
+	public void testDelete() throws Exception {
 
-	    dao.delete(1);
-	  }
+		dao.delete(1);
+	}
+
+	@Test
+	public void testListPage() throws Exception {
+
+		int page = 3;
+
+		List<BoardVO> list = dao.listPage(page);
+
+		for (BoardVO boardVO : list) {
+			logger.info(boardVO.getBno() + ":" + boardVO.getTitle());
+		}
+	}
+	
+	@Test
+	public void testListCriteria() throws Exception{
+		Criteria cri = new Criteria();
+		cri.setPage(2);
+		cri.setPerPageNum(20);
+		
+		List<BoardVO> list = dao.listCriteria(cri);
+		for(BoardVO boardVO : list) {
+			logger.info(boardVO.getBno() + ":" + boardVO.getTitle());
+		}
+	}
 
 }
